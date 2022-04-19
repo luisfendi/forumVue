610949754dd51a0700856353000778ef9072d557
luisfendi@protonmail.com <luisfendi@protonmail.com>
2022-04-19 17:49:21 +0300

    стилизация

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="profil">
-      <h2 class="profil-name">{{user?.displayName}}</h2>
+      <h1 class="profil-text">Профиль пользователя:</h1>
       <img class="profil-img" :src="user?.photoURL">
+      <h2 class="profil-name">{{user?.displayName}}</h2>
       <p class="profil-email">{{user?.email}}</p>
-      <button @click="buildIMG">img</button>
-  <p>{{user}}</p>
   </div>
 </template>
 
@@ -29,6 +28,24 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../assets/scss/general.scss';
 
+  .profil {
+    @include wrapper;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: baseline;
+    justify-content: flex-start;
+    &-img{
+      width: 100%;
+      border: 1px solid black;
+      min-width: 50vw;
+      min-height: 50vw;
+    }
+    &-name, &-email {
+      width: 50%;
+    }
+  }
 </style>
